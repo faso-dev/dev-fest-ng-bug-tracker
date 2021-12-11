f4978ef376d432ea74710b9b5624c3d380659246
Jerome Onadja <jeromeonadja28@gmail.com>
2021-12-11 12:10:34 +0000

    feat(app): first service setup

--- src/app/components/dashboard/_partials/header/header.component.ts
+++ src/app/components/dashboard/_partials/header/header.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CurrentComponentTitleService} from "../../../../services/CurrentComponentTitleService";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+    currentComponentTitle: string = '';
 
-  ngOnInit(): void {
-  }
+    constructor(private currentComponentTitleService: CurrentComponentTitleService) {
+    }
 
+    ngOnInit(): void {
+        this.currentComponentTitleService.currentComponentTitle.subscribe(newTitle => {
+            this.currentComponentTitle = newTitle
+        })
+    }
 }
